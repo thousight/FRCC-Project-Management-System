d6d7140b4f6e088b582a3176f3c2367f344b9fa5
Mark Wen <markwenguojie94@gmail.com>
2015-07-02 14:56:17 -0700

    FRCC Project Management System
    Finally get this project started with a tutorial of MEAN stack,
    hopefully this tutorial gets me what I need and I can be smart enough to
    actually twist it to what I want.

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config.js');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.listen(config.port, function(err) {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log("Listening on port 3000.");
+  }
+});
