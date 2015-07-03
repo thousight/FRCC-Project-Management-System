1c62b6348e72bc7884594e9987eacb39b38c0308
Mark Wen <markwenguojie94@gmail.com>
2015-07-02 17:33:27 -0700

    Server Side Features Added v1.7
    Login api is added. Contacted with github about the failing files, still
    haven't get response, so don't worry about them yet.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,11 +1,26 @@
 var User = require('../models/user');
 var config = require('../../config');
 var secretKey = config.secretKey;
+var jsonwebtoken = require('jsonwebtoken');
+
+// Create tokens for users with jsonwebtoken
+function createToken(user) {
+  var token = jsonwebtoken.sign({
+    _id: user._id,
+    name: user.name,
+    username: user.username
+  }, secretKey, {
+    expirtesInMinute: 1440
+  });
+
+  return token;
+}
 
 module.exports = function(app, express) {
 
   var api = express.Router();
 
+// signup api
   api.post('/signup', function(req, res) {
     var user = new User({
       name: req.body.name,
@@ -39,6 +54,35 @@ module.exports = function(app, express) {
     })
 
   })
+
+  // login api
+  api.post('/login', function(req, res) {
+
+    User.findOne({
+      username: req.body.username
+    }).select('password').exec(function(err, user) {
+
+      if(err) {
+        throw err;
+      }
+
+      if (!user) {
+        res.send({ message: "User doesn't exist"});
+      } else if(user){
+        var validPassword = user.comparePassword(req.body.password);
+        if (!validPassword) {
+          res.send({ message: "Invalid Password"});
+        } else {
+          var token = createToken(user);
+          res.json({
+            success: true,
+            message: "Login Successfully !",
+            token: token
+          });
+        }
+      }
+    });
+  });
 
   return api;
 }
